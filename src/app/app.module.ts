183b63c4165b1aeaaee03fb5e9974fe8cfc5566c
Btof <bt12of@gmail.com>
2020-07-12 09:05:24 +0300

    angular conversion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MatSelectModule } from '@angular/material/select';
+
 import { AppComponent } from './app.component';
+import { ProductsModule } from './products/products.module';
+import { CoreModule } from './core/core.module';
+import { HomeModule } from './shared/home.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ProductsModule,
+    CoreModule,
+    HomeModule,
+    BrowserAnimationsModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
